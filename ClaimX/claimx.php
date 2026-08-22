b0552e44d60f5b2a12c028c8bcc36635bfe70349
Scripti-Fy <kishanishad2024@gmail.com>
2026-08-22 08:07:07 +0530

    Add files via upload

---
+++ ClaimX/claimx.php
@@ -0,0 +1,107 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Generated PHP protected loader.
+ */
+
+$_x = [
+    8 => 'vUQMLNEooiW29uKHlU+tGYTX9uZJ59yFF76KoRhWeBHuYbCzjUXabF47u6j8iIziEK3ceyZeoGdQ9nP/57gI5pojyCNyh152zKc3sdScZxoLB259DvemHFxZYz/5VgS0dcqySBO/WRYiRkJlvc3ynxM5s3pctMK+oZrLv5tYfFdBeax1mSG5',
+    23 => 'IIUtCnjK4vZ/8tGYijT8clZ8J07hzYhQpoI5E+OpyZnIH5ByB3IaKvdsXIL/4rqs1B81frBHQ1DlRPBO9KwP/m6ZGmippKKf4gsvihJsYTIgbU8YHaROcdmaYb1G7IWjSywYBwEOnj7ENYC0dxkoc2kJfoHfE6EvJPpjCb0VVRo3EOSOyTft',
+    12 => 'dQGXSod8pFJ2DuFHiRU6xKUrRWbYPNQjETcRGo53BxdjtJi02FF9EC5k3rKPzeQpUkXtzyhaYfAE4M9U7+IGGo5iRMjaEmVIrtDqe5VMvaK2+QooJ7tcc0jdAZE+X+ijO2+s6fiT4qD2JRyKk1GljvOM5OdSikfPa4aE27FpwNn+VpcCqoLX',
+    2 => '9JVhjoTOM+V9qbOyF6xqYXZIAUWlXGey+ZaB8iJhQ/sWMqlSvK8GSkrFNuaw6ZGxjpQb1DnIaRYkNdF9Dn9+q+QTLD/GuCBNXeHLWxfd330wzuMaORFcLU9iKbWV+jyEWEzkBbqXxdE07+0qTNfDzxcNsPqoFIQh6ZstovkybtIvh/qjl1MW',
+    28 => 'fOU/Thsqhso6ivdMajTCRKJdV7dMfAKzEVvdTJxWE4SKTjCWBC9D1l6CLGYMuX9K0zp8CZ15ARLlwFJMU5KEDRBctJ5vv6PINygbQvsW3c28GROjPjn2vr8bZQeuemRFbA8jcOMsYqh+LPK8l17PoykR+o37wA+e/Yb8uRB4Cy7cGOzcaCI8',
+    9 => '4cFRHXKqjR5oK+bAlpieuXBDIduUL7RaEnmFPqtC6PgYEt4iZVSy7mRLrstaQIpwxRCUFB3+vQkwv1k9IUZ+5/ApkXW3ZInup3kANfCGu+OUPaU49cAN0cxIyXx4Hlx/3g7wXRBg/TpkmPyruG4SFS951WAAbKT4y2/firhCluSAXCqlFIlo',
+    21 => 'HYF8o3IRXG2F0WhP8PrSvDaGVK0o3oWHRhKjJSAsLQrwlfSd7QRyWSbD+PhPbxOqAN7IVWqjhsoiMQfILCX7pt1w/rjI92XhQTTLqGtp6tLhAdUEjlVB92sWDvmXBsu51CHPNHPQm4Fcyo96xXsHv4WAugIvEPXp3j2Nph5Q53X6eMjRwh5Q',
+    1 => '0fyn9zm87SKfeLOLk/ofcG57HhMMDhtH3X29nQgJja9OKv8L/oP8GbwssBjO0hRk2KK7WKAIOQADfRfMpmZTEGIrHbDslchS2tzpgEFNfarcPttWTU+iwhJ7pvNVq76JZypnIznqAZ88fGZTe8jp2L4ONRZFYKUHFaiaX87Xl1yR3md973iX',
+    20 => 'MJ8lENin/kXar4gRqQebKbSI6TN/pbn+iq+Wq48carQYdrovSphueOZVnB5KnaTga7CVHE7DTGt9rOJaaOAwZYqqa6JGCX0PImhA87tIXe32bSSX8CJdYU2RHoB4FgLEb4csolnuv46Lxoco8w9yYhh1XHdFZRkfB35JS1PfKXI6CXQZPgUc',
+    25 => 'W0ZR5XE+Jwg3YWDIpoaMi9x9JfSeQDBSqzJ2vzwEICp6Aulw6Xzt8sswE2QCoQOZJjlAGPeprd/Alh/nl1inFeNToP/iGNCynC860ichw5BPSiO0O+YFBONv7P/58NatddRhGc5Pd1OwhoBPHfux3dCfXn/Ztj8y7GsYSVevlLSUe6ggZ2ZZ',
+    11 => 'Hz1ho+ji9rj9FgVdzQnQkrSGDHPPDTwjweIKhMZkYas6+2lOvUGRqeupZrFfQpCb+mGPVMu+vcAC55xJY2Omdm4tPlUgEXCp0t3ooDv4h4oClwp9HSzbxeCRXtNfhN43IBCowNmFjDXIE7nql5IZq3QZn3DM1LfQN3X2yovHWbKhXPU5Oq/r',
+    4 => 'ezZPp446Ksk4PNLze6tytDkjpEsH/XNOKo8sF7aO+vEoFrS1J//UeFm4calHOQnpLR61XMehP5GKVTf9DeHsVcV/FXYecBPEr+OCsSktSe5pmSsNQquIBIFO06qWdHBFwA/OsvT9h4gk0UQUFhCM8xodQOGeKVPWitQC1bUJS6MmoeuZqiHt',
+    6 => '8jhs/crpXIdTArl+wuUZO8NCrIFpzHCL7Dc2XyIy+TK89fsC33iJhIeGnEYC7z5FzNLd75tgmetz44PBnpjy/AmetTOJg98DJuNmycss6o+L01Luyiv6VgiwzdQ1MNL2VflHQMYfkycf5MHeXbhINNnpay5Dghy+7XqEGOnGjt3C5BOJfmb5',
+    15 => 'jyv9dqPcXytm0Ai4zy7jLzqjZ6kg1AwC+BsGczsCn+0TZ8g6sSq5IvhFit7ctKn+8bcjdqKNSuL9FZird45wPaFjfsE2rbeqiOl6hn98XSi7UNKl1cr3SU9P4F02FBjpohH3SgdZKxRnkfM2h72z46bb4ABK38TkbJ7q99/iECuQU5PzPbkp',
+    17 => 'WhvtQjLTHt9yHaIU5iqwOHG6ufJnvys6N99+rJ0Va92hiBirrBi8Vhve0NJRhxpl3CglFTruB8xPgQTWcAJlCSZA9beSbn3RCJ31kWEBRF7i3O3Xzo04suMQfrUwvLQUe9mxmuOYI3qd05CJQ1EbE5L9cIAuNS2V4ox3/1OOHwCnfDvHHZRv',
+    14 => 'EcRT+CYZ+7xpYO1+FodLt2BOhBt8IT0Ke28B112cdyjRVSJuD89sWOgPcPBk1OALGvaTXYZIKegU80h9ckKKxubDEjDcki411sGpTEqJ8+L9gdBdXCawKekknBy/WoNS592lDlRIZ4YJc6Adx0oDZpCwh8+CezUZknqBJVbTlaliXckBugQP',
+    10 => 'WQO2Y0/CQg6Q6qH0Iwjyf7XlP7RdoEKK4bFf8Vd+0n0RK8R88vy+Qy9VW9cF8253z0bnF3oTuhG7ynOdkF4+aZpSevDUfGq7Zu0VxTl/xQxYn+A6swVbEgG8YnbVbBIqenJeZEhr9PAW+iEAjGPMlZqnz9IzgfRHt8oAShMS8KEc2SPfjQsZ',
+    29 => '754JpiU+skCH4b63U3llw7nR2nbkpaVm6wrL8tu/an/02pqSKZlrMvN0cWK02qk5kwa0c7lFwK4/YYk1f7/NBpi3Q4TjTyUnbuA43qm5tt+60m2ZaEsMUHGGrfIxwfff0YuuH7CsDgkhmBTFTY/WRZWqjotb9m1Zsza7F+w8jSD9l6o28zXu',
+    18 => 'RJjC40W0tI4OauNwjtZuYLjkhbleTIpQ9Wd1ie6zmo1a2bjcye0SyDF7H/ML1MFWpLEVbh1PeghXMLG8kbt7Svcj0yD4loMwQWKYMCfnKgbQHksZS4Q4a/nRWMyp7jAHtomZwFCsNka1rgTwtFOhXcbsQl7Smk7I5FSd0tjsk9wqAaMfHBfV',
+    22 => 'q6kBX/I6mc/8BCuZ3e48773u+os01z3lwywR7pYR9aL5gQTyqqBUpxqj4xWtjygMJHkK1x/2ajOsSUqLJ6po00jBN0x1tFIJl2Xfu2AnGihLM8FptV6LE7w3cq55TUdOJO0qr7vMwAe4LTrIj87DmxkDNVt/Vd81nhn/6SYLK8VqlahMJohi',
+    30 => 'MeNuDVItSEAMwT/b7A==',
+    16 => 'OxLR4dMNxqo25bH5UuHIF2dGlyyPzXBqY2CGZ7rF7bG/u2l2hSjyxZnL5HOSzzkIqmyrJYha6IAdzusdxmmclfpNrJRzQPoAgluO8uQkxI6S1tm1Sn/x2KzjOnP+fAORE41QkRc0Q0EhPhFTUUUotZtG2WywJDS56tgKn9LpMuVBhAil919E',
+    27 => 't7pEWIHPTqF59LYZzMShC33pw7uKB7JNetNg87drLIJYYPV6Czzr4uamOPUdh9Ws5QXJR5UGH9mzNyu5tEMcyA3II6JjAgZHyDwV0rVDJ7q1FZQ+rLYlAJBUsSiQQTspZK7BYH/ytYXcjrwzPIWRo1aC7zR2QXVfkwQ5ydGgiQI9VtHvkFEx',
+    3 => 'dXtXbwdrFCWo9QWni2RFJRrlvbsNLcvwDZK4jO+wVmUfoYgVOrO+QVEKtBij/bNgEYbWBq55Esxc2m42654VzLRCH9fB018glcJvw5eVikmk6YTsGkYQmsQfNG8HwNj+WDxc+bnd3xI/XliLlSQSqLh2LR87/FFjAPuCa2NlJkgnuSheaCxC',
+    24 => 'bVFYDyynBWHzHWP42lOOBXzM+kx0HyE0dehyl3BgMSCuKqvYpXPc5ziJ0ktvyUPBzJDZXKvbOX545isMlVBRSGfNNZAuNSzqWfxtV+gjsE7sy6XrJwxy/1h47mZB1cIlrfdxib8njFYiDhD/vHUaA9KZMpKLTRtL8mbGveI9DBWTZRC+JgXc',
+    7 => 'EzsBr9DXtO4Q+KYx+//IuuvdR/Rd7lHNhhcRRevveY2rxWm3aeaPN3ZpFBgJaYuJo9WD7x4tvgdDmtG9UvIWeq5xXKUzV43i3A/0t5g/iQUQ+GAxmzHUlnE5ngK2OOGcahsTB3llvXIgSqHJFLlPL5hk1BynQzwsg0d7dMrMP0bcmZDZ3t+P',
+    0 => 'N/O2jN8eOWdrxNGS8byX/c9Ee+4gVK5foL1ERrpGSYXbgaBvQlCoBRfi2u+jAIvrdBzKVTJi9uICsfmVxIV8e0FuBvXs53FkEsf/Cf+KpaZOJDmzr19aIQOiWCmxcbXq6S83kAv6k29dIffAZ5kQs6yIBptlPMuM/x6phxV7NFpvA1SIEs2o',
+    13 => 'uUZZVWte5mwJVNaY4K4MC+Fdct+o7dUuUUn4U9S//QWqHbDntHAU7mh1xfWby72cSIGU2QtAATJ1sPn1r6vUZx/kDsJdbV1Hwnz7jOZ33IBVLXMRM54sbZX9X51h8ihT1UR/Md2qdynkHRrQxUyTG+VE4J9H8j1dCcnZQJVnaKjIOru51F/d',
+    19 => 'kzQbodngAR5WKKgbMAMokAMNeMvPgT8OBwGbtQkx5IZBavSh4fs5b9XYMEPyjRp4qKjH2cX57UGAc65tfb0hjtXuFcJkzqdKKentFY+HS8leA4yWYZSHhbb/kC1Qf1eXTUSXc1Rmz/U3Qv4kmqqBCYDM3s9aRz0xAA+NOUuDTb5RZIcJCrZR',
+    26 => 'wXQXYMfyjqVcxXBs7xG2Yg+3ZngNqr0ZLTf+gHKhBRYbtJ7hWhGpyu6ChO/TO8/y28p4ASdjuzaJYEokWAuqTZrFZzYKiarQKU9I5OnxdlUgvSrNFiK2QUpYouw83vQw5cKUMmhkdUCBjvX1DL4BIvnDdKWIRFnGyudDiSM9BLPrCxZvfvK9',
+    5 => 'Yg/g6AQO+i3kwozXV70YdKM4D6ZJrP9SOJAlz84l95QcEea1HiqfRA6k5EmULeGunX5JfmFVMvjhmlpo8ZUwLeG1JtPjw+tTQTCkC4WCxg1Uk8wmI0jcjqgUldT27slTooQVY+tAdOObsZnLsphVMI+0yu8CRl8EQLpeWKAjX4ZrV0pzdO9e',
+
+];
+
+$_k = '2sjr48THTIiR0QYWE6VN6YYdzn1owrtxlY/94PLE7Xk=';
+
+/*
+ * Restore original chunk order.
+ */
+
+ksort($_x);
+
+$_p = '';
+
+foreach ($_x as $_v) {
+    $_p .= $_v;
+}
+
+/*
+ * Base64 decode.
+ */
+
+$_p = base64_decode($_p, true);
+
+if ($_p === false) {
+    die("Payload error.\n");
+}
+
+/*
+ * Decode key.
+ */
+
+$_k = base64_decode($_k, true);
+
+if ($_k === false || $_k === '') {
+    die("Key error.\n");
+}
+
+/*
+ * XOR decode.
+ */
+
+$_o = '';
+
+$_kl = strlen($_k);
+$_pl = strlen($_p);
+
+for ($_i = 0; $_i < $_pl; $_i++) {
+    $_o .= $_p[$_i] ^ $_k[$_i % $_kl];
+}
+
+/*
+ * Decompress.
+ */
+
+$_src = gzinflate($_o);
+
+if ($_src === false) {
+    die("Decompression error.\n");
+}
+
+/*
+ * Execute original PHP.
+ */
+
+unset($_x, $_k, $_p, $_o);
+
+eval('?>' . $_src);
